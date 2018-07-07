a42f71ed62ba520976ae90954edef9a90e21fd25
louis0727 <31844163+louis0727@users.noreply.github.com>
2018-07-07 10:30:18 +0800

    Update create-app.sh

--- create-app.sh
+++ create-app.sh
@@ -1,3 +1,1 @@
-APPNAME=person-repository
-SOURCE_REPOSITORY_URL=https://github.com/PeterWippermann/person-repository.git
-oc new-app --template payara-maven3 --param APPNAME=$APPNAME,SOURCE_REPOSITORY_URL=$SOURCE_REPOSITORY_URL
+oc new-app --template payara-maven3 --param APPNAME=person-repository,SOURCE_REPOSITORY_URL=https://github.com/PeterWippermann/person-repository.git
